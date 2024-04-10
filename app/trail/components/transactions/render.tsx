3a53d9ec6d10f8a207d75608c427f493a354d684
BNSX <bnsdev@mail.com>
2024-04-10 16:29:24 +0700

    removed `dialog`

--- app/trail/components/transactions/render.tsx
+++ app/trail/components/transactions/render.tsx
@@ -1,79 +1,19 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
 import { formatDateAndTime } from "@/lib/date";
-import { ReadonlyURLSearchParams, useRouter } from "next/navigation";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
-import { useEffect, useState } from "react";
-import { $Enums } from "@prisma/client";
+import { NumberThousand } from "@/lib/number";
 
 interface Props {
-    searchParams: ReadonlyURLSearchParams;
     data: Transaction[];
+    onClicked: (index: number) => void;
 }
-export function RenderTransaction({ data }: Props) {
-    const router = useRouter();
+export default function RenderTransaction({ data, onClicked }: Props) {
     if (data.length === 0) {
         return <p className="text-center pt-5">Data not found.</p>;
     }
-    const [value, setValue] = useState<Transaction | null | undefined>(null);
-    const [open, setOpen] = useState(false);
-    const onOpenChange = () => setOpen(!open);
-    const onClicked = (index: number) => {
-        const d = data.find((x, i) => i === index);
-        if (d === undefined) {
-            router.refresh();
-        } else {
-            setValue(d);
-            setOpen(true);
-        }
-    };
-    useEffect(() => {
-        if (open === false) {
-            setValue(null);
-        }
-    }, [open]);
+
     return (
         <div className="space-y-3">
-            <div>
-                {value && (
-                    <Dialog open={open} onOpenChange={onOpenChange}>
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>{value?.title}</DialogTitle>
-                                <DialogDescription>
-                                    {value.description || "No Comment"}
-                                </DialogDescription>
-                            </DialogHeader>
-                            <div className="space-y-1">
-                                <div>
-                                    <TypeTranslator type={value.type} />
-                                </div>
-                                <div className="flex gap-3">
-                                    <p>Amount</p>
-                                    {value.type === "expense" ? (
-                                        <p className="text-red-600">
-                                            -{value.amount}
-                                            {value.currencyCode}
-                                        </p>
-                                    ) : (
-                                        <p className="text-green-600">
-                                            +{value.amount}
-                                            {value.currencyCode}
-                                        </p>
-                                    )}
-                                </div>
-                            </div>
-                        </DialogContent>
-                    </Dialog>
-                )}
-            </div>
             {data.map((x, i) => (
                 <div
                     key={x.txid}
@@ -98,7 +38,7 @@ export function RenderTransaction({ data }: Props) {
                             }
                         >
                             {x.type === "income" ? "+" : "-"}
-                            {x.amount}
+                            {NumberThousand(x.amount)}
                             {x.currencyCode}
                         </p>
                         {x.description && (
@@ -125,27 +65,4 @@ export function RenderTransaction({ data }: Props) {
             ))}
         </div>
     );
-}
-
-function TypeTranslator({ type }: { type: $Enums.TransactionType }) {
-    switch (type) {
-        case "income":
-            return (
-                <Badge variant={"outline"} className="text-green-600">
-                    Income
-                </Badge>
-            );
-        case "expense":
-            return (
-                <Badge variant={"outline"} className="text-red-600">
-                    Expense
-                </Badge>
-            );
-        default:
-            return (
-                <Badge variant={"outline"} className="text-green-600">
-                    Income
-                </Badge>
-            );
-    }
 }
