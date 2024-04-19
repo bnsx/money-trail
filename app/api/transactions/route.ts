c1e3780af346276815792700c5921c6c9c6cab28
BNSX <bnsdev@mail.com>
2024-04-19 15:13:09 +0700

    make query with type select

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -20,17 +20,17 @@ export async function GET(req: NextRequest) {
                 { status: 401 }
             );
         }
-        
+
         let pageIndex = parseInt(
             req.nextUrl.searchParams.get("pageIndex") || "1",
             10
         );
-        
+
         let pageSize = parseInt(
             req.nextUrl.searchParams.get("pageSize") || "10",
             10
         );
-        
+
         if (!pageSizeList.includes(pageSize)) {
             pageSize = 10;
         }
@@ -42,7 +42,45 @@ export async function GET(req: NextRequest) {
         if (!typeList.includes(type)) {
             type = "all";
         }
-       
+
+        if (type !== "all") {
+            const data = await prisma.transactions.findMany({
+                where: { memberID, type, deletedAt: null },
+                select: {
+                    txid: true,
+                    title: true,
+                    amount: true,
+                    description: true,
+                    type: true,
+                    date: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    countries: { select: { currencyCode: true } },
+                    categories: { select: { categoryID: true, name: true } },
+                },
+            });
+
+            const pageCount = Math.ceil(data.length / pageSize);
+            const start = (pageIndex - 1) * pageSize;
+            const end = start + Number(pageSize);
+            const entries = data.slice(start, end).map((x) => ({
+                ...x,
+                amount: Number(x.amount),
+                currencyCode: x.countries.currencyCode,
+                category:
+                    {
+                        id: x.categories?.categoryID,
+                        name: x.categories?.name,
+                    } || null,
+                categories: undefined,
+                countries: undefined,
+            }));
+
+            return NextResponse.json(
+                { data: entries, pageIndex, pageSize, pageCount },
+                { status: 200 }
+            );
+        }
         const data = await prisma.transactions.findMany({
             where: { memberID, deletedAt: null },
             select: {
