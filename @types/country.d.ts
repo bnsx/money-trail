d13b510d841804f16c67e3cab94745a94ad503be
BNSX <bnsdev@mail.com>
2024-04-21 17:25:44 +0700

    renamed field to correct name

--- @types/country.d.ts
+++ @types/country.d.ts
@@ -2,7 +2,7 @@ interface Country {
     name: string;
     currencyCode: string;
     currencyName: string;
-    currrencySymbol: string;
+    currencySymbol: string;
     isoChar2: string;
     isoChar3: string;
     isoNumeric: number;
