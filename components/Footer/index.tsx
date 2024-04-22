324ef74d38c925eacf19a32d8072f483d4786528
BNSX <bnsdev@mail.com>
2024-04-23 00:09:05 +0700

    completed footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,3 +1,14 @@
-export function Footer(){
-    return <footer className="bg-black h-64 xl:h-96"></footer>
+"use client";
+import Link from "next/link";
+
+export function Footer() {
+    return (
+        <footer className="bg-foreground text-white w-full text-sm text-center p-3">
+            Money <span className="text-red-500">Trail</span> made by{" "}
+            <Link href={"https://github.com/bnsx"} className="font-bold">
+                BNSX
+            </Link>{" "}
+            with 🤍
+        </footer>
+    );
 }
