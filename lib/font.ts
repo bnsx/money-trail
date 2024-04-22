cf70c2acf073f9ad9595a6479af7ba4c3ccc4db1
BNSX <bnsdev@mail.com>
2024-04-22 11:29:06 +0700

    this can reusable for all space

---
+++ lib/font.ts
@@ -0,0 +1,7 @@
+import { Sarabun } from "next/font/google";
+
+export const sarabunFont = Sarabun({
+    weight: ["100", "200", "300", "400", "500", "600", "700"],
+    subsets: ["thai", "latin"],
+    style: ["normal"],
+});
