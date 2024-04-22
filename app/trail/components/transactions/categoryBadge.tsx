35d3679795015026013a37ae8db91fdba46bc870
BNSX <bnsdev@mail.com>
2024-04-22 11:26:59 +0700

    make category badge can reusable

---
+++ app/trail/components/transactions/categoryBadge.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@/components/ui/badge";
+import { sarabunFont } from "@/lib/font";
+
+interface Props {
+    data: string | null | undefined;
+}
+export function CategoryBadge({ data }: Props) {
+    return (
+        <Badge
+            className={`${sarabunFont.className} w-fit h-fit`}
+            variant={"secondary"}
+        >
+            {data || "Uncategory"}
+        </Badge>
+    );
+}
