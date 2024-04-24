de3eb123a2e88974b6df9f2373b99af83a8c66c1
BNSX <bnsdev@mail.com>
2024-04-24 11:06:49 +0700

    completed

---
+++ app/api/member/deactivate/route.ts
@@ -0,0 +1,33 @@
+import { member } from "@/lib/member";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const token = await getToken({ req });
+        const memberID = token?.id as string;
+        const hasMember = await member.hasMember({ memberID });
+        if (!hasMember || hasMember.status === false) {
+            return NextResponse.json(
+                {
+                    message: "Unauthorized!",
+                    code: "UNAUTHORIZED",
+                },
+                { status: 401 }
+            );
+        }
+        await member.deactivate({ memberID });
+        return NextResponse.json(
+            {
+                message: "Deactivated member",
+                code: "SUCCESS",
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Internal Server Error", code: "INTERNAL_SERVER_ERROR" },
+            { status: 500 }
+        );
+    }
+}
