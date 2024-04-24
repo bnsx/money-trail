bd19c9d6a123a974dc1c74d6d1c82b018978e23d
BNSX <bnsdev@mail.com>
2024-04-24 10:46:07 +0700

    added `text-xs` for support mobile

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <footer className="bg-foreground text-white w-full text-sm text-center p-3">
-            Money <span className="text-red-500">Trail</span> made by{" "}
-            <Link href={"https://github.com/bnsx"} className="font-bold">
+        <footer className="bg-foreground text-white w-full text-xs xl:text-sm text-center p-3">
+            <span className="font-semibold">Money</span>{" "}
+            <span className="font-semibold text-red-500">Trail</span> made by{" "}
+            <Link
+                target="_blank"
+                href={"https://github.com/bnsx"}
+                className="font-bold"
+            >
                 BNSX
             </Link>{" "}
             with 🤍
