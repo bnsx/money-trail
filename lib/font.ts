553164293b40b5badb3d01ed246d2f2a2f7dd1fa
Ammarin <bnsdev@mail.com>
2024-06-24 17:21:20 +0700

    updated font to `IBM Plex Sans Thai`

--- lib/font.ts
+++ lib/font.ts
@@ -1,7 +1,6 @@
 import { IBM_Plex_Sans_Thai } from "next/font/google";
 
-export const defaultFont = IBM_Plex_Sans_Thai({
+export const fontStyle = IBM_Plex_Sans_Thai({
     weight: ["100", "200", "300", "400", "500", "600", "700"],
     subsets: ["thai", "latin"],
-    style: ["normal"],
 });
